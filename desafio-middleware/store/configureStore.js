c5c23e8054e7f076c12a48a534e91575fccb4141
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-10-09 23:05:40 -0300

    redux middleware

--- desafio-middleware/store/configureStore.js
+++ desafio-middleware/store/configureStore.js
@@ -5,7 +5,7 @@ import token from './token.js';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk, localStorage));
-const reducer = combineReducers({ token });
+const reducer = combineReducers({ token, user });
 const store = createStore(reducer, enhancer);
 
 export default store;
